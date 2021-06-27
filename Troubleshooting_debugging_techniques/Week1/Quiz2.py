eeb3f71dbb8801eac504a50000e83f40bf6189c2
Yawo Amengonu <amengonu12@gmail.com>
2021-06-26 22:27:43 -0400

    this quiz2

---
+++ Troubleshooting_debugging_techniques/Week1/Quiz2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 26 21:42:48 2021
+
+@author: yhame
+"""
+
+import re
+def compare_strings(string1, string2):
+  #Convert both strings to lowercase
+  #and remove leading and trailing blanks
+  string1 = string1.lower().strip()
+  string2 = string2.lower().strip()
+
+  #Ignore punctuation
+  punctuation = r"[.?!,;:']"
+  string1 = re.sub(punctuation, "", string1)
+  string2 = re.sub(punctuation, "", string2)
+
+  #DEBUG CODE GOES HERE
+  #print(___)
+
+  return string1 == string2
+def main():
+    output = compare_strings("Have a Great Day!", "Have a great day?")  # True
+    #print(compare_strings("It's raining again.", "its raining, again"))  # True
+    #print(compare_strings("Learn to count: 1, 2, 3.", "Learn to count: one, two, three."))  # False
+    #print(compare_strings("They found some body.", "They found somebody."))  # False
+    return print(output)
+if __name__=="__main__":
+    main()
